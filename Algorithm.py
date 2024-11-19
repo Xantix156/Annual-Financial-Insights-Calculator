3135a285a2cdcb5722ab2d56b38b57ee875d9f18
Santiago Fleitas Pérez <santiflepe@gmail.com>
2024-11-19 22:57:56 +0000

    Update Algorithm.py

--- Algorithm.py
+++ Algorithm.py
@@ -1,3 +1,17 @@
+# TEST GIST FOR THE SUBJECT HPI
+"""
+Copyright (C) 2024 Team: Not_Found
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
 def analyze_finances(expenses, monthly_salary):
     # Initialize total expenses and a dictionary to store category-wise totals
     total_spent = 0
